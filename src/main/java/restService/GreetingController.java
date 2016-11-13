2f0bea46c8c23a8c43255251da08a74588e57322
Michiel Van Den Berghe <michiel.vandenberghe@student.odisee.be>
2016-11-13 16:06:53 +0100

    CrossOrigin for all domains

--- src/main/java/restService/GreetingController.java
+++ src/main/java/restService/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    @CrossOrigin(origins = "https://student-dating-app-mojito.herokuapp.com")
+    @CrossOrigin
     @RequestMapping("/hello")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
