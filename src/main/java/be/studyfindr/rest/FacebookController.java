dfb8e346dbd9fd5a21073abeae7f41b0a7488184
anthony mestdach <anthony.mestdach@student.odisee.be>
2016-11-20 17:55:39 +0100

    add comments

--- src/main/java/be/studyfindr/rest/FacebookController.java
+++ src/main/java/be/studyfindr/rest/FacebookController.java
@@ -4,22 +4,38 @@ import java.io.IOException;
 import java.util.HashMap;
 import javax.servlet.http.HttpSession;
 
+import be.studyfindr.entities.ErrorResponse;
 import be.studyfindr.entities.LoginResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+/**
+ * The FacebookController defines the interface for all Facebook operations.
+ * @version 1.1
+ */
 @RestController
 public class FacebookController {
- 	private FacebookLogic fb;
+ 	// connection to the logic
+	private FacebookLogic fb;
 
-	public FacebookController() throws IOException {
+	/**
+	 * Creates an instance of the FacebookController
+	 */
+	public FacebookController() {
 		fb = new FacebookLogic();
   	}
 
   	//https://www.facebook.com/dialog/oauth?client_id=1794346987494326&redirect_uri=http://localhost:8080/auth/facebook
-  	@RequestMapping("/auth/facebook")
+
+	/**
+	 * Handles a Facebook login by accepting an authentication token and returning an access token and user ID.
+	 * This method expects following usage: http://myhost.com/auth/facebook?code=<Auth. token>
+	 * @param session the auth. token
+	 * @return will redirect to the Facebook api to request the access token
+	 */
+	@RequestMapping("/auth/facebook")
   	public RedirectView facebookAuth(HttpSession session) {
 	  	try{
 		  	return fb.startAuthentication(session);
@@ -28,19 +44,17 @@ public class FacebookController {
 	  	}
   	}
 
+	/**
+	 * Handles the Facebook login callback. This method should not be used directly by a user.
+	 * @param code response from Facebook
+	 * @param state session state
+	 * @param session callback session
+	 * @return Login response, will contain the access token and user ID on success.
+	 */
   	@RequestMapping("/auth/facebook/callback")
   	public LoginResponse callBack(@RequestParam("code") String code, @RequestParam("state") String state, HttpSession session) {
 	  	return fb.callBack(code, state, session);
   	}
-
-	@RequestMapping("/facebook/getmyinfo")
-	public ResponseEntity<be.studyfindr.entities.User> getMyInfo(@RequestParam("accessToken") String accessToken, @RequestParam("id") long id) throws RestException {
-		be.studyfindr.entities.User s = fb.getMyInfoFromBackend(accessToken, id);
-		if(s == null){
-			throw new RestException("Invalid login");
-		}
-		return new ResponseEntity<be.studyfindr.entities.User>(s, HttpStatus.OK);
-	}
 
 	@RequestMapping("/facebook/logout")
 	public HashMap<String, String> logout(@RequestParam("accessToken") String accessToken, @RequestParam("id") long id) throws RestException {
