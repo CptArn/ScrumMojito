ebb659fd6140f1c35d5bc758be3902c1b07ccf1f
anthony mestdach <anthony.mestdach@student.odisee.be>
2016-11-27 10:51:01 +0100

    Add tests for userController

--- src/test/java/be/studyfindr/UserTest.java
+++ src/test/java/be/studyfindr/UserTest.java
@@ -34,4 +34,43 @@ public class UserTest {
         assert(u1.getAge() == 20 && u1.getEmail() == "email@gmail.com" && u1.getFirstname() == "Jos" && u1.getLastname() == "Jansens" && u1.getLocation() == "Gent"
                 && u1.getid() == 42 && u1.getPrefAgeMin() == 18 && u1.getPrefAgeMax() == 35 && u1. getPrefDistance() == 25 && u1.getPrefLocation() == 2);
     }
+
+    @Test
+    public void test2Clone() {
+        User u2 = u1.clone();
+        assert(u2.equals(u1));
+    }
+
+    @Test
+    public void test3Hash() {
+        User u2 = u1.clone();
+        u2.setAge(12);
+        int hash1 = u1.hashCode();
+        int hash2 = u2.hashCode();
+        assert(hash1 != hash2);
+    }
+
+    @Test
+    public void test4SetGender() {
+        User u2 = u1.clone();
+        u2.setIsFemale();
+        boolean res1 = u2.getIsMale() ^ u2.getIsFemale();
+        u2.setIsMale();
+        boolean res2 = u2.getIsMale() ^ u2.getIsFemale();
+        u2.setIsTrans();
+        boolean res3 = u2.getIsTrans() ^ u2.getIsFemale();
+        assert(res1 && res2 && res3);
+    }
+
+    @Test
+    public void test5DefaultContruct() {
+        User u2 = new User();
+        assert(u2.getFirstname() == null);
+    }
+
+    @Test
+    public void test6ToString() {
+        String res = u1.toString();
+        assert(res.contains("firstname"));
+    }
 }
--- src/test/java/be/studyfindr/UsersControllerClass.java
+++ src/test/java/be/studyfindr/UsersControllerClass.java
@@ -1,26 +1,28 @@
 package be.studyfindr;
 
 import be.studyfindr.entities.Data;
-import be.studyfindr.entities.Message;
 import be.studyfindr.entities.User;
-import be.studyfindr.rest.FacebookController;
 import be.studyfindr.rest.UsersController;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.util.JSON;
 import org.bson.Document;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.springframework.http.HttpOutputMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -36,6 +38,12 @@ public class UsersControllerClass {
 
     UsersController controller;
 
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
     @BeforeClass
     public static void setUp() {
         u1 = new User(1, "email@email.com", "Jan", "Peeters", "Oiljst", 18, true, false, true, 18, 35, 25, 1, false, false);
@@ -47,6 +55,7 @@ public class UsersControllerClass {
     public void setUpMockMVC() {
         try {
             controller = new UsersController();
+            mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -108,19 +117,49 @@ public class UsersControllerClass {
         }
     }
 
-    /*@Test
-    public void test1PutInfo() {
+    @Test
+    public void test6UpdateSuccess() {
         try {
-            String json = u1.toString();
-            MvcResult result = this.mockMvc.perform(put("/user/1/update")
-                    .contentType(MediaType.APPLICATION_JSON).content(json)
-            ).andExpect(status().isOk()).andReturn();
-            User user = new User(Document.parse(result.getResponse().getContentAsString()));
-            assert(user.equals(u1));
+            User newUser = u1.clone();
+            newUser.setFirstname("newName");
+            mockMvc.perform(post("/user/1/update")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("accessToken", "testtoken")
+                    .content(this.json(newUser))
+                    .accept(MediaType.APPLICATION_JSON)
+
+            ).andExpect(status().is2xxSuccessful());
+            User fromBackend = dataLayer.getUser(1);
+            assert(fromBackend.getFirstname().equals("newName"));
+
         } catch(Exception e) {
             System.out.println(e.getMessage());
         }
-    }*/
+    }
+
+    @Test
+    public void test7UpdateNotFound() {
+        try {
+            User newUser = u1.clone();
+            newUser.setFirstname("newName");
+            mockMvc.perform(post("/user/-1/update")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("accessToken", "testtoken")
+                    .content(this.json(newUser))
+                    .accept(MediaType.APPLICATION_JSON)
+
+            ).andExpect(status().isNotFound());
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
 
     @AfterClass
     public static void tearDown() {
