d242f064bd6966c2d2c1a7c28dd1c368f34b84a1
anthony mestdach <anthony.mestdach@student.odisee.be>
2016-12-06 11:01:40 +0100

    add comments

--- src/main/java/be/studyfindr/rest/MessageController.java
+++ src/main/java/be/studyfindr/rest/MessageController.java
@@ -2,32 +2,37 @@ package be.studyfindr.rest;
 
 import be.studyfindr.entities.Data;
 import be.studyfindr.entities.Message;
-import be.studyfindr.entities.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by anthony on 4/12/2016.
- */
+/*
+* The UserController defines the interface for all User operations.
+* @version 1.0
+*/
 public class MessageController {
     Data dataLayer = new Data();
     private FacebookLogic fb;
 
     /**
-     * Inits. the UserController
+     * Inits. the MessageController
      */
     public MessageController(){
         fb = new FacebookLogic();
     }
 
+    /**
+     * Returns a list of all messages between the own user and a match.
+     * @param id own id
+     * @param accessToken valid access token
+     * @param matchId id of the match
+     * @return list of messages
+     */
     @RequestMapping("/messages/getconversation")
     public ResponseEntity<List<Message>> getConversation(@RequestParam("id") long id, @RequestParam("accessToken") String accessToken, @RequestParam("matchid") long matchId) {
         if (!fb.userIsValid(accessToken, id)) return new ResponseEntity<List<Message>>(HttpStatus.UNAUTHORIZED);
@@ -38,7 +43,14 @@ public class MessageController {
         }
     }
 
-
+    /**
+     * Sends a message to a match
+     * @param id own id
+     * @param accessToken valid access token
+     * @param matchId id of the matched user
+     * @param message message to send
+     * @return send message
+     */
     @RequestMapping(path = "/messages/postmessage", method = RequestMethod.POST)
     public ResponseEntity<Message> postMessage(@RequestParam("id") long id, @RequestParam("accessToken") String accessToken, @RequestParam("matchid") long matchId, @RequestBody String message) {
         if (!fb.userIsValid(accessToken, id)) return new ResponseEntity<Message>(HttpStatus.UNAUTHORIZED);
--- src/main/java/be/studyfindr/rest/SchoolController.java
+++ src/main/java/be/studyfindr/rest/SchoolController.java
@@ -10,9 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The SchoolController defines the interface for all School operations.
+ * @version 1.0
+ */
 @RestController
 public class SchoolController {
-    Data dataLayer = new Data();
+    // connection to database
+    private Data dataLayer = new Data();
+
+    /**
+     * Returns a list of all schools.
+     * @param name Optional query school name
+     * @return all found schools
+     */
     @CrossOrigin
     @RequestMapping("/schools")
     public List<School> getAllSchools(@RequestParam(value="name", required=false) String name) {
